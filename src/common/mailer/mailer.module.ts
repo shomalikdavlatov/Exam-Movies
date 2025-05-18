bdc13b1fa3e64f405657a075c311f9af5297f4a8
shomalikdavlatov <shomalikdavlatov@gmail.com>
2025-05-19 03:29:01 +0500

    Email verification added

---
+++ src/common/mailer/mailer.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { MailerModule as NestMailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { MailerService } from './mailer.service';
+
+@Module({
+  imports: [
+    NestMailerModule.forRootAsync({
+      useFactory: async () => ({
+        transport: {
+          host: process.env.MAIL_HOST,
+          port: parseInt(process.env.MAIL_PORT!),
+          secure: false,
+          auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASS,
+          },
+        },
+        defaults: {
+          from: process.env.MAIL_FROM,
+        },
+        template: {
+          dir: process.cwd() + '/templates',
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }),
+  ],
+  providers: [MailerService],
+  exports: [MailerService],
+})
+export class MailerModule {}
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   ConflictException,
   Injectable,
   InternalServerErrorException,
@@ -9,14 +10,19 @@ import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { JwtService } from '@nestjs/jwt';
 import bcrypt from 'bcrypt';
+import { RedisService } from 'src/common/redis/redis.service';
+import { MailerService } from 'src/common/mailer/mailer.service';
+import { VerificationDto } from './dto/verification.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
     private jwtService: JwtService,
     private prisma: PrismaService,
+    private redis: RedisService,
+    private mailer: MailerService,
   ) {}
-  async register(body: RegisterDto, avatar: object | undefined) {
+  async register(body: RegisterDto) {
     const userUsername = await this.prisma.user.findFirst({
       where: {
         username: body.username,
@@ -35,9 +41,52 @@ export class AuthService {
       throw new ConflictException(
         'There is already a user with a specified email!',
       );
+    const code = Math.floor(100000 + Math.random() * 900000);
+    await this.mailer.sendVerificationCode(
+      body.email,
+      'Verification code',
+      code,
+    );
+    await this.redis.set(
+      `verify:${body.email}`,
+      JSON.stringify({ ...body, code }),
+      600,
+    );
+    return {
+      message: `Verification code sent to ${body.email}. You have 10 minutes to verify your account or all information will be deleted!`,
+    };
+  }
+  async verify(body: VerificationDto) {
+    const stored = await this.redis.get(`verify:${body.email}`);
+    if (!stored) throw new BadRequestException('Code expired or not found!');
+    const userData = JSON.parse(stored);
+    if (userData.code != body.code)
+      throw new BadRequestException('Invalid code!');
+    await this.redis.del(`verify:${body.email}`);
+    const userUsername = await this.prisma.user.findFirst({
+      where: {
+        username: userData.username,
+      },
+    });
+    if (userUsername)
+      throw new ConflictException(
+        'There is already a user with a specified username!',
+      );
+    const userEmail = await this.prisma.user.findFirst({
+      where: {
+        email: userData.email,
+      },
+    });
+    if (userEmail)
+      throw new ConflictException(
+        'There is already a user with a specified email!',
+      );
     if (!process.env.HASH) throw new InternalServerErrorException();
-    const hashedPassword = await bcrypt.hash(body.password, +process.env.HASH);
-    const { password, ...rest } = body;
+    const hashedPassword = await bcrypt.hash(
+      userData.password,
+      +process.env.HASH,
+    );
+    const { code, password, ...rest } = userData;
     const user = {
       ...rest,
       password_hash: hashedPassword,
@@ -65,7 +114,6 @@ export class AuthService {
         username: userResponse.username,
         role: userResponse.role,
         created_at: userResponse.created_at,
-        avatar_url: avatar ? avatar['path'] : null,
       },
     };
   }
